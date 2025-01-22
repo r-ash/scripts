27a7696c3eab73e18a68d96f8e4d44dc8423beea
Rob Ashton <robertashton94@gmail.com>
2025-01-22 17:42:35 +0000

    Make git_cleanup work with repos with default branch main or master

--- git_cleanup.sh
+++ git_cleanup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Tidy up git branches which have been merged into master
 
+default_branch=$(git rev-parse --abbrev-ref origin/HEAD | cut -c8-)
 current_branch=$(git branch | grep \* | cut -d ' ' -f2)
-if [[ $current_branch != master ]]; then
-        echo "Switching to master branch"
-        git checkout master
+if [[ $current_branch != $default_branch ]]; then
+        echo "Switching to $default_branch branch"
+        git checkout $default_branch
 	git pull
 fi
 
-branches=$(git branch --merged=master | grep -v master)
+branches=$(git branch --merged=$default_branch | grep -v $default_branch)
 
 if [[ $branches ]]; then
 	echo -e "Cleaning up branches\n $branches"
